d48923f32b3710badfe7a33ba6804e1910fb9b88
Etienne Vaillancourt <e.vaillancourt@hotmail.ca>
2018-03-13 15:51:04 -0400

    Création de test unitaires pour le nom de la personne qui parle du problème

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProblemeComponent } from './probleme.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 describe('ProblemeComponent', () => {
   let component: ProblemeComponent;
@@ -8,6 +10,7 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[ReactiveFormsModule,AngularFontAwesomeModule],
       declarations: [ ProblemeComponent ]
     })
     .compileComponents();
@@ -19,7 +22,57 @@ describe('ProblemeComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('Zone PRÉNOM invalide avec 2 caractères', () => {
+      let zone = component.problemeForm.controls['nomProbleme'];
+      zone.setValue('a'.repeat(2));
+      expect(zone.valid).toBeFalsy();
+    });
+
+  it('Zone PRÉNOM valide avec 3 caractères', () => {
+    let zone = component.problemeForm.controls['nomProbleme'];
+    zone.setValue('a'.repeat(3));
+    expect(zone.valid).toBeTruthy();
+  });
+
+
+
+  it('Zone PRÉNOM valide avec 200 caractères', () => {
+    let zone = component.problemeForm.controls['nomProbleme'];
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it('Zone PRÉNOM invalide avec aucune valeur', () => {
+    let errors = {};
+    let zone = component.problemeForm.get('nomProbleme');
+    zone.setValue('');
+    errors = zone.errors || {};
+    expect(errors['minLength']).toBeTruthy;
+  });
+
+  it('Zone PRÉNOM invalide avec 1 caractère', () => {
+    let errors = {};
+    let zone = component.problemeForm.get('nomProbleme');
+    zone.setValue('a');
+    errors = zone.errors || {};
+    expect(errors['minLength']).toBeTruthy;
+  });
+
+  it('Zone PRÉNOM valide avec 50 espaces', () => {
+    let zone = component.problemeForm.controls['nomProbleme'];
+    zone.setValue(' '.repeat(50));
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it('Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+    let zone = component.problemeForm.controls['nomProbleme'];
+    zone.setValue('  a');
+    expect(zone.valid).toBeTruthy();
+  });
 });
+
+
