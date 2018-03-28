5a4d1edd7c4335821d295f9510ae940bcf8359b3
Etienne Vaillancourt <e.vaillancourt@hotmail.ca>
2018-03-28 14:05:07 -0400

    Ajouter vérifications champ type

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -20,7 +20,7 @@ export class ProblemeComponent implements OnInit {
   ngOnInit() {
     this.problemeForm = this.fb.group({
       prenomProbleme: ['',[VerifierCaracteresValidator.sansEspaces(),VerifierCaracteresValidator.longueurMinimum(3)]],
-      noType: ['']
+      noType: ['',Validators.required]
     });
 
     this.types.obtenirTypes()
